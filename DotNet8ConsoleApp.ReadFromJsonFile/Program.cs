be95ea3d629db906d1601c7885ac6513653ea27e
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-09 22:54:21 +0630

    Add project files.

---
+++ DotNet8ConsoleApp.ReadFromJsonFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DotNet8ConsoleApp.ReadFromJsonFile;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        IConfigurationRoot configuration = builder.Build();
+
+        //var connStr = configuration["ConnectionStrings:DbConnection"];
+        var connStr = configuration.GetConnectionString("DbConnection");
+        Console.WriteLine(connStr);
+    }
+}
